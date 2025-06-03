d4cb8acf4a128aa72f58958f272b488d070d32bb
jokalzz <jokakalzz@gmail.com>
2025-06-03 23:48:26 +0800

    mengubah send_message

--- send_message.php
+++ send_message.php
@@ -14,7 +14,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
         <title>Pesan Terkirim</title>
         <link rel='icon' href='./assets/images/silvasuphaa.png' type='image/png'>
-        <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'>
         <link rel='stylesheet' href='./assets/css/send_message.css'>
     </head>
     <body>
@@ -25,10 +24,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <p><strong>Pesan:</strong> $message</p>
             <a href='javascript:history.back()' class='btn btn-primary'>Kembali ke Kontak</a>
         </div>
-
-        <script src='https://code.jquery.com/jquery-3.5.1.slim.min.js'></script>
-        <script src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js'></script>
-        <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'></script>
     </body>
     </html>";
 } else {
